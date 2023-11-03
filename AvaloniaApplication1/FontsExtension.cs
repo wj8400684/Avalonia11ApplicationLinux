7f825ed088fd09975a16e5c7b033ac835809556b
wujun <8400684@qqcom>
2023-11-04 00:12:36 +0800

    Initial commit

---
+++ AvaloniaApplication1/FontsExtension.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Avalonia;
+using Avalonia.Media;
+using Avalonia.Media.Fonts;
+
+namespace AvaloniaApplication1;
+
+public static class FontsExtension
+{
+    public static class FontSettings
+    {
+        public static Uri Key { get; } = new Uri("fonts:MicrosoftYaHei", UriKind.Absolute);
+        public static Uri Source { get; } = new Uri("avares://AvaloniaApplication1/Assets/Fonts", UriKind.Absolute);
+    }
+
+    public static class AlibabaFontSettings
+    {
+        public static Uri Key { get; } = new Uri("fonts:AlibabaPuHuiTi", UriKind.Absolute);
+        public static Uri Source { get; } = new Uri("avares://WHOrigin.FrontDesk/Assets/Fonts/AliBaba", UriKind.Absolute);
+    }
+
+    public static class LcdFontSettings
+    {
+        public static Uri Key { get; } = new Uri("fonts:LCD", UriKind.Absolute);
+        public static Uri Source { get; } = new Uri("avares://WHOrigin.FrontDesk/Assets/Fonts/LCD", UriKind.Absolute);
+    }
+    
+    public static AppBuilder UseFontFamily([DisallowNull] this AppBuilder builder)
+    {
+        builder.With(new FontManagerOptions
+        {
+            DefaultFamilyName = "avares://AvaloniaApplication1/Assets/Fonts/my.ttf#Microsoft YaHei",
+            FontFallbacks = new[]
+            {
+                new  FontFallback
+                {
+                    FontFamily = new FontFamily("avares://AvaloniaApplication1/Assets/Fonts/my.ttf#Microsoft YaHei")
+                }
+            }
+        });
+        return builder.ConfigureFonts(manager => manager.AddFontCollection(new EmbeddedFontCollection(FontSettings.Key, FontSettings.Source)))
+            //.ConfigureFonts(manager => manager.AddFontCollection(new EmbeddedFontCollection(AlibabaFontSettings.Key, AlibabaFontSettings.Source)))
+            //.ConfigureFonts(manager => manager.AddFontCollection(new EmbeddedFontCollection(LcdFontSettings.Key, LcdFontSettings.Source)));
+            ;
+    }
+}
